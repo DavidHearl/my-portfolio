aeed9b4ae7cefc0b8c3d61da4ee224fd58f7033e
DavidHearl <david.hearl@outlook.com>
2022-01-18 17:30:26 +0000

    updated cv page

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 ## Recources Used
 https://material.io/design/color/dark-theme.html
 https://www.befunky.com/learn/light-and-dark-themes/
+
+https://uxplanet.org/create-an-easily-switchable-light-dark-styles-in-figma-ffee3cd542a7
--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,24 +1,24 @@
-let mode = "dark"
+// let mode = "dark"
 
-$("#theme").on("click", function() {
-    if (mode == "dark") {
+// $("#theme").on("click", function() {
+//     if (mode == "dark") {
 
-        mode = "light"
-        $(this).text("Click here to come back to the light side !")
-        $(this).css("background-color", "white");
-        $(this).css("color", "#121212");
-        $(".navbar").css("background-color", "black");
-        $(".navbar").css("color", "white");
-        $("#navigation").removeClass("navbar-light").addClass("navbar-dark");
+//         mode = "light"
+//         $(this).text("Click here to come back to the light side !")
+//         $(this).css("background-color", "white");
+//         $(this).css("color", "#121212");
+//         $(".navbar").css("background-color", "black");
+//         $(".navbar").css("color", "white");
+//         $("#navigation").removeClass("navbar-light").addClass("navbar-dark");
 
-    } else {
+//     } else {
 
-        mode = "dark"
-        $(this).text("Click here to join the dark side !")
-        $(this).css("background-color", "#121212");
-        $(this).css("color", "white");
-        $(".navbar").css("background-color", "#be4a4a");
-        $(".navbar").css("color", "white");
-        $("#navigation").removeClass("navbar-dark").addClass("navbar-light");
-    }
-})
+//         mode = "dark"
+//         $(this).text("Click here to join the dark side !")
+//         $(this).css("background-color", "#121212");
+//         $(this).css("color", "white");
+//         $(".navbar").css("background-color", "#be4a4a");
+//         $(".navbar").css("color", "white");
+//         $("#navigation").removeClass("navbar-dark").addClass("navbar-light");
+//     }
+// })
