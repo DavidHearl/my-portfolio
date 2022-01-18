624a73f52df8c309f94df38f56055f89fe0fddfd
DavidHearl <david.hearl@outlook.com>
2022-01-18 15:45:51 +0000

    changed navigation menu and added dark mode

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,19 +1,24 @@
-let mode = "light"
+let mode = "dark"
 
-$("#theme").on("click", function () {
+$("#theme").on("click", function() {
+    if (mode == "dark") {
 
-    if (mode == "light") {
-
-        $(this).css("color", "red");
-        $(this).css("background-color", "black");
-        mode = "dark"
+        mode = "light"
+        $(this).text("Click here to come back to the light side !")
+        $(this).css("background-color", "white");
+        $(this).css("color", "#121212");
+        $(".navbar").css("background-color", "black");
+        $(".navbar").css("color", "white");
+        $("#navigation").removeClass("navbar-light").addClass("navbar-dark");
 
     } else {
 
-        $(this).css("color", "black");
-        $(this).css("background-color", "white");
-        mode = "light"
-
+        mode = "dark"
+        $(this).text("Click here to join the dark side !")
+        $(this).css("background-color", "#121212");
+        $(this).css("color", "white");
+        $(".navbar").css("background-color", "#be4a4a");
+        $(".navbar").css("color", "white");
+        $("#navigation").removeClass("navbar-dark").addClass("navbar-light");
     }
-
 })
