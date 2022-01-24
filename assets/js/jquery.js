18ef8febefee4ee3e392e07b909b4337cc8a12f1
DavidHearl <david.hearl@outlook.com>
2022-01-24 10:23:00 +0000

    updated some styles for dark mode

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,5 +1,11 @@
 var mode = "dark"
 
+// Background colors
+let blue = "repeating-linear-gradient(135deg, rgb(30,100,150) 6px, rgb(40, 110, 160) 12px)";
+let white = "repeating-linear-gradient(135deg, rgb(240, 240, 240) 6px, rgb(230, 230, 230) 12px)";
+let black = "repeating-linear-gradient(135deg, rgb(40, 40, 40) 6px, rgb(30, 30, 30) 12px)";
+let gray = "repeating-linear-gradient(135deg, rgb(60, 60, 60) 6px, rgb(50, 50, 50) 12px)";
+
 // This obviouisly changed the settings of the darkmode button, set it to change text first
 $(".toggle").on("click", function() {
 
@@ -9,18 +15,15 @@ $(".toggle").on("click", function() {
         $(this).text("Dark Mode");
 
         // Lightmode - Background Color:
-        // Blue: 30,100,150 - 40,110,160
-        // White: 230,230,230 - 240,240,240
-        $(".navbar").css("background", "repeating-linear-gradient(135deg, rgb(30,100,150) 6px, rgb(40, 110, 160) 12px)");
-        $("footer").css("background", "repeating-linear-gradient(135deg, rgb(30,100,150) 6px, rgb(40, 110, 160) 12px)");
-        $(".list-group-item").css("background", "repeating-linear-gradient(135deg, rgb(60, 60, 60) 6px, rgb(50, 50, 50) 12px)");
-        $(".card").css("background", "repeating-linear-gradient(135deg, rgb(30,100,150) 6px, rgb(40, 110, 160) 12px)");
-        $(".card-body").css("background", "repeating-linear-gradient(135deg, rgb(30,100,150) 6px, rgb(40, 110, 160) 12px)");
-        $(".accordion-item").css("background", "repeating-linear-gradient(135deg, rgb(30,100,150) 6px, rgb(40, 110, 160) 12px)");
-        $(".accordion-button").css("background", "repeating-linear-gradient(135deg, rgb(30,100,150) 6px, rgb(40, 110, 160) 12px)");
-
-        $(".background").css("background", "repeating-linear-gradient(135deg, rgb(240, 240, 240) 6px, rgb(230, 230, 230) 12px)");
-        $(".dropdown-menu").css("background", "repeating-linear-gradient(135deg, rgb(240, 240, 240) 6px, rgb(230, 230, 230) 12px)");
+        $(".navbar").css("background", blue);
+        $("footer").css("background", blue);
+        $(".list-group-item").css("background", blue);
+        $(".card").css("background", blue);
+        $(".card-body").css("background", blue);
+        $(".accordion-item").css("background", blue);
+        $(".accordion-button").css("background", blue);
+        $(".background").css("background", white);
+        $(".dropdown-menu").css("background", white);
 
         // Text Color
         $("h1").css("color", "#000000");
@@ -29,9 +32,11 @@ $(".toggle").on("click", function() {
         $("p").css("color", "#000000");
         $("li").css("color", "#000000");
 
+        //
+        $("hr").css("background-color", "black");
+
         // Variables
         mode = "light";
-
 
     } else {
 
@@ -39,18 +44,16 @@ $(".toggle").on("click", function() {
         $(this).text("Light Mode");
 
         // Darkmode - Background Color:
-        // Black: 40,40,40 - 30,30,30
-        // Gray: 60,60,60 - 50,50,50
-        $(".background").css("background", "repeating-linear-gradient(135deg, rgb(40, 40, 40) 6px, rgb(30, 30, 30) 12px)");
-        $(".dropdown-menu").css("background", "repeating-linear-gradient(135deg, rgb(40, 40, 40) 6px, rgb(30, 30, 30) 12px)");
-        $(".card-body").css("background", "repeating-linear-gradient(135deg, rgb(40,40,40) 6px, rgb(30, 30, 30) 12px)");
+        $(".navbar").css("background", gray);
+        $("footer").css("background", gray);
+        $(".list-group-item").css("background", gray);
+        $(".card").css("background", gray);
+        $(".card-body").css("background", gray);
+        $(".accordion-item").css("background", gray);
+        $(".accordion-button").css("background", gray);
 
-        $(".navbar").css("background", "repeating-linear-gradient(135deg, rgb(60, 60, 60) 6px, rgb(50, 50, 50) 12px)");
-        $("footer").css("background", "repeating-linear-gradient(135deg, rgb(60, 60, 60) 6px, rgb(50, 50, 50) 12px)");
-        $(".list-group-item").css("background", "repeating-linear-gradient(135deg, rgb(60, 60, 60) 6px, rgb(50, 50, 50) 12px)");
-        $(".card").css("background", "repeating-linear-gradient(135deg, rgb(60, 60, 60) 6px, rgb(50, 50, 50) 12px);");
-        $(".accordion-item").css("background", "repeating-linear-gradient(135deg, rgb(60, 60, 60) 6px, rgb(50, 50, 50) 12px)");
-        $(".accordion-button").css("background", "repeating-linear-gradient(135deg, rgb(60, 60, 60) 6px, rgb(50, 50, 50) 12px)");
+        $(".background").css("background", black);
+        $(".dropdown-menu").css("background", black);
 
         // Text Color
         $("h1").css("color", "#e2e2e2");
@@ -58,6 +61,9 @@ $(".toggle").on("click", function() {
         $("h3").css("color", "#e2e2e2");
         $("p").css("color", "#e2e2e2");
         $("li").css("color", "#e2e2e2");
+
+        //
+        $("hr").css("background-color", "white");
 
         // Variables
         mode = "dark";
