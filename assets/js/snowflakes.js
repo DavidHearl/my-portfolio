b3096833a14815b9494f94e18ff81c9f23b3257c
DavidHearl <david.hearl@outlook.com>
2023-05-16 23:17:44 +0100

    added console loggging for the values, added more keyframes

--- assets/js/snowflakes.js
+++ assets/js/snowflakes.js
@@ -8,12 +8,19 @@ document.addEventListener('DOMContentLoaded', function () {
   
     for (let i = 0; i < numberOfSpheres; i++) {
       const sphereElement = document.createElement('span');
-      const randomValue = Math.floor(Math.random() * 45) + 5;
-      const randomDuration = Math.random() * 5 + 5; // Adjust the range here for faster animation
+      const randomValue = Math.floor(Math.random() * 45) + 9;
+      const randomDuration = Math.random() * 5 + 4; // Adjust the range here for faster animation
       const randomDelay = Math.random() * 5; // Adjust the range here for delay
+      const randomSway = Math.random() * 15 - 1; // Adjust the range here for sway
+  
+      console.log(`Sphere ${i + 1} - Random Value: ${randomValue}`); // Unitless
+      console.log(`Sphere ${i + 1} - Random Duration: ${randomDuration}`); // Value in s
+      console.log(`Sphere ${i + 1} - Random Delay: ${randomDelay}`); // Value in s
+      console.log(`Sphere ${i + 1} - Random Sway: ${randomSway}`); // Value in px
+
       sphereElement.setAttribute(
         'style',
-        `--i:${randomValue}; --duration:${randomDuration}s; --delay:${randomDelay}s`
+        `--i:${randomValue}; --duration:${randomDuration}s; --delay:${randomDelay}s; --sway:${randomSway}px`
       );
       spheresContainer.appendChild(sphereElement);
     }
