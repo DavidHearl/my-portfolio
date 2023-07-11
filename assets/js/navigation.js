79d84f43b7958bf7cdb4605aacf36e895106d660
DavidHearl <david.hearl@outlook.com>
2023-07-11 23:21:52 +0100

    Added spans and animation to the hamburger icon, basic animation works but there are minor bugs when clicking slightly in the incorrect positions. Does not hamper site functionality however it effects user experience

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -7,22 +7,22 @@ $(document).ready(function () {
 
     // Update the active navigation item based on the section in view
     function updateActiveNavItem() {
-      var currentScrollPos = $(window).scrollTop() + navHeight;
+        var currentScrollPos = $(window).scrollTop() + navHeight;
 
-      sectionIds.forEach(function (sectionId) {
-        var sectionTop = $(sectionId).offset().top;
-        var sectionHeight = $(sectionId).outerHeight();
+        sectionIds.forEach(function (sectionId) {
+            var sectionTop = $(sectionId).offset().top;
+            var sectionHeight = $(sectionId).outerHeight();
 
-        if (currentScrollPos >= sectionTop && currentScrollPos < sectionTop + sectionHeight) {
-          $('.navbar-nav li').removeClass('active');
-          $('a[href="' + sectionId + '"]').closest('li').addClass('active');
-        }
-      });
+            if (currentScrollPos >= sectionTop && currentScrollPos < sectionTop + sectionHeight) {
+                $('.navbar-nav li').removeClass('active');
+                $('a[href="' + sectionId + '"]').closest('li').addClass('active');
+            }
+        });
     }
 
     // On scroll, update the active navigation item
     $(window).scroll(function () {
-      updateActiveNavItem();
+        updateActiveNavItem();
     });
 
     // On page load or refresh, update the active navigation item
@@ -30,9 +30,22 @@ $(document).ready(function () {
 
     // Smooth scrolling to section when a navigation link is clicked
     $('a.nav-link').click(function (event) {
-      event.preventDefault();
-      var target = $(this).attr('href');
-      var offset = $(target).offset().top - navHeight + 1;
-      $('html, body').animate({ scrollTop: offset }, 500);
+        event.preventDefault();
+        var target = $(this).attr('href');
+        var offset = $(target).offset().top - navHeight + 1;
+        $('html, body').animate({ scrollTop: offset }, 500);
     });
-  });
+});
+
+$(document).ready(function () {
+    // Close navbar on link click
+    $('.navbar-nav > li > a').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+});
+
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
