1b369bee4622a535cf57b5af1511e2cc90dbe579
DavidHearl <david.hearl@outlook.com>
2023-07-11 22:43:54 +0100

    Fixed the hamburger menu not opening, added JavaScript to change the navigation highlighing as the user moves through the tabs

---
+++ assets/js/navigation.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    // Get the height of the navigation bar
+    var navHeight = $('nav').outerHeight();
+
+    // Store section IDs in an array
+    var sectionIds = ['#home', '#about', '#skills', '#projects', '#work', '#education', '#contact'];
+
+    // Update the active navigation item based on the section in view
+    function updateActiveNavItem() {
+      var currentScrollPos = $(window).scrollTop() + navHeight;
+
+      sectionIds.forEach(function (sectionId) {
+        var sectionTop = $(sectionId).offset().top;
+        var sectionHeight = $(sectionId).outerHeight();
+
+        if (currentScrollPos >= sectionTop && currentScrollPos < sectionTop + sectionHeight) {
+          $('.navbar-nav li').removeClass('active');
+          $('a[href="' + sectionId + '"]').closest('li').addClass('active');
+        }
+      });
+    }
+
+    // On scroll, update the active navigation item
+    $(window).scroll(function () {
+      updateActiveNavItem();
+    });
+
+    // On page load or refresh, update the active navigation item
+    updateActiveNavItem();
+
+    // Smooth scrolling to section when a navigation link is clicked
+    $('a.nav-link').click(function (event) {
+      event.preventDefault();
+      var target = $(this).attr('href');
+      var offset = $(target).offset().top - navHeight + 1;
+      $('html, body').animate({ scrollTop: offset }, 500);
+    });
+  });
